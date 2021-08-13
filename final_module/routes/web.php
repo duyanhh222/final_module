43e9b9084aacf6dbfc081f3121a35b0a2cb03413
Tran Van An <an1102ramos@gmail.com>
2021-08-13 10:42:51 +0700

    ok

--- final_module/routes/web.php
+++ final_module/routes/web.php
@@ -21,14 +21,15 @@ Route::group(['prefix' => 'admin'],function(){
     Route::get('/login','Admin\AdminController@index')->name('admin.login');
     Route::post('/login','Admin\AdminController@check')->name('admin.check.login');
     Route::get('/dashboard','Admin\AdminController@dashboard')->name('admin.dashboard');
-    Route::group(['prefix' => 'category'],function(){
-        Route::get('','Admin\CategoryController@index')->name('category.index');
+
+        Route::get('category','Admin\CategoryController@index')->name('category.index');
         Route::get('/{category}/edit','Admin\CategoryController@edit')->name('category.edit');
         Route::get('/{category}','Admin\CategoryController@destroy')->name('category.destroy');
-        Route::get('/create','Admin\CategoryController@create')->name('category.create');
+
         Route::post('/store','Admin\CategoryController@store')->name('category.store');
         Route::post('/{category}','Admin\CategoryController@update')->name('category.update');
-    });
+
+    Route::get('category/create','Admin\CategoryController@create')->name('category.create');
 });
 
 
