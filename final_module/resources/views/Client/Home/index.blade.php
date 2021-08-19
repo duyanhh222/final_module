2f603a024a82043fc4aca32b54cc39d86d95b4d2
Nguyen Trung Thu <nguyenthu180100@gmail.com>
2021-08-19 17:30:05 +0700

    xoa menu home

--- final_module/resources/views/Client/Home/index.blade.php
+++ final_module/resources/views/Client/Home/index.blade.php
@@ -144,56 +144,6 @@
                 </div>
             </div>
 
-            <!-- Main Navigation -->
-
-            <nav class="main_nav">
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-
-                            <div class="main_nav_content d-flex flex-row">
-
-                                <!-- Categories Menu -->
-
-                                <div class="cat_menu_container">
-                                    <div class="cat_menu_title d-flex flex-row align-items-center justify-content-start">
-                                        <div class="cat_burger"><span></span><span></span><span></span></div>
-                                        <div class="cat_menu_text">Danh mục</div>
-                                    </div>
-
-                                    <ul class="cat_menu">
-                                        @foreach($categories as $category)
-                                        <li><a href="{{ route('client.category', $category->id) }}">{{ $category->name }} <i class="fas fa-chevron-right"></i></a></li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-
-                                <!-- Main Nav Menu -->
-
-                                <!-- <div class="main_nav_menu ml-auto">
-                                    <ul class="standard_dropdown main_nav_dropdown">
-                                        <li><a href="">Home<i class="fas fa-chevron-down"></i></a></li>
-                                        <li><a href="blog.html">Blog<i class="fas fa-chevron-down"></i></a></li>
-                                        <li><a href="">Contact<i class="fas fa-chevron-down"></i></a></li>
-                                    </ul>
-                                </div> -->
-
-                                <!-- Menu Trigger -->
-
-                                <div class="menu_trigger_container ml-auto">
-                                    <div class="menu_trigger d-flex flex-row align-items-center justify-content-end">
-                                        <div class="menu_burger">
-                                            <div class="menu_trigger_text">menu</div>
-                                            <div class="cat_burger menu_burger_inner"><span></span><span></span><span></span></div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
         </header>
     @show
 
