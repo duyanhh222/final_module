7327d058aaaf1eb8483febb9823186c0e8278bbe
Tran Van An <an1102ramos@gmail.com>
2021-08-20 15:57:48 +0700

    up config to home, index

--- final_module/app/Models/Config.php
+++ final_module/app/Models/Config.php
@@ -9,4 +9,5 @@ class Config extends Model
 {
     use HasFactory;
     protected $primaryKey = 'id';
+
 }
--- final_module/resources/views/Admin/Config/edit.blade.php
+++ final_module/resources/views/Admin/Config/edit.blade.php
@@ -39,34 +39,160 @@
                     @enderror
                 </div>
             </div>
-            <div class="row">
+
+            <div class="row g-3">
                 <div class="col-6 input_group">
-                    <label for="">Banner 1</label>
-                    <input type="file" name="banner1"  class="form-control" id="upload" placeholder="">
-                    <input type="hidden" name="file_file" value="{{$config->banner1}}" >
+                    <label class="custom-file-upload">Logo
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="logo"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_logo" value="{{$config->logo}}" >
+                    </label>
                     @error('file')
                     <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
                 </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->logo)}}" width="30px" />
+                </div>
             </div>
-            <div>
-                <img id="blah" src="{{asset('Client/images/'.$config->banner1)}}" width="150px" />
-            </div>
-
-
-            <div class="row">
+            <div class="row g-3">
                 <div class="col-6 input_group">
-                    <label for="">Logo</label>
-                    <input type="file" name="logo"  class="form-control" id="upload" placeholder="">
-                    <input type="hidden" name="file_file" value="{{$config->logo}}" >
+                    <label class="custom-file-upload">Banner 1
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="banner1"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_banner1" value="{{$config->banner1}}" >
+                    </label>
                     @error('file')
                     <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
                 </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->banner1)}}" width="150px" />
+                </div>
             </div>
-            <div>
-                <img id="blah" src="{{asset('Client/images/'.$config->logo)}}" width="30px" />
+
+            <div class="row g-3">
+                <div class="col-6 input_group">
+                    <label class="custom-file-upload">Banner 2
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="banner2"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_banner2" value="{{$config->banner2}}" >
+                    </label>
+                    @error('file')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->banner2)}}" width="150px" />
+                </div>
             </div>
+
+            <div class="row g-3">
+                <div class="col-6 input_group">
+                    <label class="custom-file-upload">Banner 3
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="banner3"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_banner3" value="{{$config->banner3}}" >
+                    </label>
+                    @error('file')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->banner3)}}" width="150px" />
+                </div>
+            </div>
+
+            <div class="row g-3">
+                <div class="col-6 input_group">
+                    <label class="custom-file-upload">Char 1
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="char1"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_char1" value="{{$config->char1}}" >
+                    </label>
+                    @error('file')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->char1)}}" width="30px" />
+                </div>
+
+                <div>
+                    <input type="text" name="char_title1" value="{{$config->char_title1}}" class="form-control" id="" placeholder="">
+
+                </div>
+
+            </div>
+
+
+            <div class="row g-3">
+                <div class="col-6 input_group">
+                    <label class="custom-file-upload">Char 2
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="char2"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_char2" value="{{$config->char2}}" >
+                    </label>
+                    @error('file')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->char2)}}" width="30px" />
+                </div>
+
+                <div>
+                    <input type="text" name="char_title2" value="{{$config->char_title2}}" class="form-control" id="" placeholder="">
+
+                </div>
+
+            </div>
+
+            <div class="row g-3">
+                <div class="col-6 input_group">
+                    <label class="custom-file-upload">Char 3
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="char3"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_char3" value="{{$config->char3}}" >
+                    </label>
+                    @error('file')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->char3)}}" width="30px" />
+                </div>
+
+                <div>
+                    <input type="text" name="char_title3" value="{{$config->char_title3}}" class="form-control" id="" placeholder="">
+
+                </div>
+
+            </div>
+
+
+            <div class="row g-3">
+                <div class="col-6 input_group">
+                    <label class="custom-file-upload">Char 4
+                        <i class="fas fa-images fa-1x"></i>
+                        <input type="file" name="char4"  class="form-control" id="upload" placeholder="">
+                        <input type="hidden" name="file_char4" value="{{$config->char4}}" >
+                    </label>
+                    @error('file')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <img id="blah" src="{{asset('storage/images/'.$config->char4)}}" width="30px" />
+                </div>
+
+                <div>
+                    <input type="text" name="char_title4" value="{{$config->char_title4}}" class="form-control" id="" placeholder="">
+
+                </div>
+
+            </div>
+            <p>Cập nhật lần cuối ngày:<strong>{{ $config->updated_at }}</strong> </p>
 
             <div>
                 <button type="submit" class="btn btn-primary">Submit</button>
