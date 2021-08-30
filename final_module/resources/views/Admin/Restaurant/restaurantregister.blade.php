633dafbde21d558bae9d697cdb86e6f085cd7382
Tran Van An <an1102ramos@gmail.com>
2021-08-30 08:17:18 +0700

    fix

--- final_module/resources/views/Admin/Restaurant/restaurantregister.blade.php
+++ final_module/resources/views/Admin/Restaurant/restaurantregister.blade.php
@@ -26,7 +26,9 @@
                 <tr>
                     <td>{{$restaurant->id}}</td>
                     <td>{{$restaurant->name}}</td>
-                    <td>{{$restaurant->user->user_name}}</td>
+                    <td>
+{{--                        {{$restaurant->user->user_name}}--}}
+                    </td>
                     <td>{{$restaurant->phone}}</td>
                     <td>
                         @if($restaurant->status == 1)
