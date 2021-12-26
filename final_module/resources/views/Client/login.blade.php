5ec341d4db38acb7e529ac168ad35232174b559d
An <an.tran@amela.vn>
2021-12-26 11:11:32 +0700

    add

--- final_module/resources/views/Client/login.blade.php
+++ final_module/resources/views/Client/login.blade.php
@@ -4,7 +4,7 @@
 	<title>Đăng nhập</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="{{ asset('Login/images/icons/favicon.ico') }}"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/vendor/bootstrap/css/bootstrap.min.css') }}">
@@ -14,13 +14,13 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/vendor/animate/animate.css') }}">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/vendor/css-hamburgers/hamburgers.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/vendor/animsition/css/animsition.min.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/vendor/select2/select2.min.css') }}">
-<!--===============================================================================================-->	
+<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/vendor/daterangepicker/daterangepicker.css') }}">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset('Login/css/util.css') }}">
@@ -28,25 +28,25 @@
 <!--===============================================================================================-->
 </head>
 <body style="background-color: #666666;">
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				
+
 				<form class="login100-form validate-form" method="post" action="{{ route('client.login') }}">
                     @csrf
 					<span class="login100-form-title p-b-43">
 						Đăng nhập
 					</span>
-					
-					
+
+
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Email</span>
 					</div>
-					
-					
+
+
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="password">
 						<span class="focus-input100"></span>
@@ -83,20 +83,20 @@
                             @endforeach
                         @endif
                     </div>
-					
+
 					@if (Session::has('login_fail'))
 						<div class="login-fail">
 							<p class="text-danger">{{ Session::get('login_fail') }}</p>
 						</div>
 					@endif
-					
+
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+						<button class="login100-form-btn" id="btnLogin">
 							Đăng nhập
 						</button>
 					</div>
-					
+
 					<div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
 							Hoặc tiếp tục với
@@ -119,11 +119,11 @@
 			</div>
 		</div>
 	</div>
-	
-	
 
-	
-	
+
+
+
+
 <!--===============================================================================================-->
 	<script src="{{ asset('Login/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
 <!--===============================================================================================-->
